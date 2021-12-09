4d962be99ff4a0d081547662fb1a58d198f15cde
lexus654 <ronald.laz@tup.edu.ph>
2021-12-09 22:44:00 +0800

    fiox open and close modal add transition

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+"use strict";
 // on touch pop up the screen
+console.log("hello");
+const menu = document.getElementById("nav__menu");
+const list = document.getElementById("nav__list");
+
+menu.addEventListener("click", () => {
+  list.style.display = "flex";
+});
+
+window.onclick = function (e) {
+  if (e.target == list) {
+    list.style.display = "none";
+  }
+};
